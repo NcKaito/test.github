5513afa7591a24c97dc4c1d33fe601eabe1593cc
darenorcan <d.orcan@cci-formation.nc>
2021-02-18 14:33:06 +1100

    .\dissier.html

---
+++ tableau.py
@@ -0,0 +1,9 @@
+names = ['Tamata', 'Nicolas', 'Marie-Claude', 'Oriane', 'Florian', 'Yvanne', 'Jordan', 'Joyce', 'Julien', 'Clément', 'Jeremy', 'Daren', 'Mikaele']
+
+print('1', names)
+
+def arraySort(myArray):
+    print('2', myArray)    
+        
+
+print(arraySort(names))
