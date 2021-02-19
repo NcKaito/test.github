2b8438f1b7b5316810e2607f9e7a002423892696
darenorcan <d.orcan@cci-formation.nc>
2021-02-19 13:21:08 +1100

    dissier.html

--- arraysearch.py
+++ arraysearch.py
@@ -2,12 +2,10 @@
 words = ['book', 'pencil', 'pen', 'note book', 'sharpener', 'rubber'] 
 
 
-def arraySearch(array, search):
-    return  True
-
-for words in word: 
-    if words == Search
-        return true
+def arraySearch(words, search):
+    for word in words: 
+        if word == search:
+            return True
 
 
 print('Vrai' if arraySearch(words, 'note book') else 'Faux')
--- tableau.py
+++ tableau.py
@@ -1,9 +1,8 @@
 names = ['Tamata', 'Nicolas', 'Marie-Claude', 'Oriane', 'Florian', 'Yvanne', 'Jordan', 'Joyce', 'Julien', 'Clément', 'Jeremy', 'Daren', 'Mikaele']
 
-print('1', names)
 
 def arraySort(myArray):
-    print('2', myArray)    
-        
+    print('1', myArray)    
+    print(sorted(myArray))
 
 print(arraySort(names))
